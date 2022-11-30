b847c09a196adcb43b167e5feae4d20a75fa072d
amos <amosjshinkle@gmail.com>
2022-11-30 15:34:17 -0500

    commit tree f238e31cbca4235d04d1d0f72f26f43e7d89f81c

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solutions in this file
+  for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+//next
+
+  const gifts = ["teddy bear", "drone", "doll"];
+
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+  
+    return gifts;
+  }
+
+  wrapGifts(gifts);
+
+//next
+
+  const names = ["Guadalupe", "Ollie", "Aki"]
+  const typeOfGift = `surprise`
+
+  function writeCards(names, typeOfGift) {
+    let messages = [];
+    for(let i=0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${typeOfGift} gift!`);
+        
+    } 
+    return messages
+    }
+  
+  const messages = []
+
+  function countDown(i) { 
+    
+    while(i >=0) {
+      console.log(i);
+    i--;
+    }
+    return i;
+}
+
+  countDown(4);
+
